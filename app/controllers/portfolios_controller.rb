b1d9cbfd2f5dfa61a3074c3cab1d598c21593ca2
kshiflett88 <kodishiflett@gmail.com>
2023-04-06 13:15:08 -0700

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,31 @@ class PortfoliosController < ApplicationController
   def index
     @portfolios = Portfolio.all
   end 
+
+  def new
+    @portfolio = Portfolio.new
+  end
+
+  def create
+    @portfolio = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio.save
+        format.html { redirect_to portfolios_path, notice: "Portfolio was successfully created." }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_portfolio
+      @portfolio = Portfolio.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def portfolio_params
+      params.require(:portfolio).permit(:title, :subtitle, :body)
+    end
 end
