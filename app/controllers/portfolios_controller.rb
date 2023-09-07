f662595f6a162a1990fabba5a903b5e037dec8a0
kshiflett88 <kodishiflett@gmail.com>
2023-09-07 15:23:44 -0400

    implemented partials with local data

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -28,9 +28,12 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
+    @portfolio = Portfolio.find(params[:id])
   end
 
   def update
+    @portfolio = Portfolio.find(params[:id])
+    
     respond_to do |format|
       if @portfolio.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: "Portfolio was successfully updated." }
